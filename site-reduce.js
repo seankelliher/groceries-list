62c0e52c2a52ea06d75fd6f17b51e96fb4abdf72
Sean Kelliher <sbkelliher@icloud.com>
2018-04-19 17:18:17 -0400

    Add namespacing

--- site-reduce.js
+++ site-reduce.js
@@ -1,135 +1,156 @@
-//ensure DOM is loaded
+//Global "variable" (actually "let")
+let market;
+
+//Ensure the DOM is loaded
 document.addEventListener("DOMContentLoaded", function () {
-  "use strict";
+    "use strict";
 
-    //clear text from input boxes when selected
-    document.getElementById("input-grocery").addEventListener("focusin", function (element) {
-        element.target.textContent = "";
-    });
+    //Invoke functions with event listeners
+    market.readyBoxes();
+    market.readyKeys();
+    market.removeTheRow();
 
-    //trigger function when return/enter key is pressed
-    document.getElementById("input-grocery").addEventListener("keydown", function (event) {
+}); //close DOMContentLoaded function
 
-        if (event.keyCode === 13) {
-            event.preventDefault();
-            checkTheNumbers();
-        }
-    });
+//The "market" "variable" (actually "let")
+market = {
 
+    readyBoxes: function () {
+        "use strict";
 
-    //check if grocery has name and price
-    function checkTheNumbers () {
+        //Monitor the table for "focus" event. Clear the "add grocery name" and "add grocery price" placeholder values from boxes when event occurs.
+        document.getElementById("input-grocery").addEventListener("focusin", function (element) {
+            element.target.textContent = "";
+        });
+    },
 
-        //retrieve entry for name and cost
-        var name = document.getElementById("input-grocery-name").textContent;
-        var cost = document.getElementById("input-grocery-cost").textContent;
+    readyKeys: function () {
+        "use strict";
 
-        //run the regex text() function on cost
-        var costReCheck = /^\d*\.?\d{1,2}$/.test(cost);
+        //Monitor the keyboard for "keydown" event
+        document.getElementById("input-grocery").addEventListener("keydown", function (event) {
 
-        //run if statement
+            //If the "keydown" event is on the "return" key, prevent its default action, invoke the retrieveTheNumbers function
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                //market.retrieveTheNumbers();
+                market.checkInsertContent();
+            }
+        });
+    },
+
+    checkInsertContent: function () {
+        "use strict";
+
+        //Retrieve the "grocery name" and grocery price" the user entered into the field
+        const name = document.getElementById("input-grocery-name").textContent;
+        const cost = document.getElementById("input-grocery-cost").textContent;
+
+        //Run regex on the "grocery price" to check it's all numbers and has no more than two digits after the decimal point
+        const costReCheck = /^\d*\.?\d{1,2}$/.test(cost);
+
+        //If "grocery name" is blank and/or "grocery price" does not match pattern, return alert. If both are fine, return the name and price.
         if (costReCheck === false) {
             window.alert("Hey, that's not a price!");
         } else if (name === "") {
             window.alert("Hey, you need a name for that grocery!");
         } else {
-            retrieveTheNumbers(name, cost); //pass name and cost to next function
+
+            //Convert "grocery price" to a number. Make format .XX in case use enters "X no decimal" or "X.X" (one number). Convert number back to string to insert in cell.
+            //Note: There's probably a better, more elegant regex way to do this.
+            const costNum = Number(cost);
+            const costNumFix = costNum.toFixed(2);
+            const costNumFixString = costNumFix.toString();
+
+            //Retrieve the table, insert a row and three cells into it
+            const groceryTable = document.getElementById("output-grocery-list");
+            const groceryRow = groceryTable.insertRow(-1); //-1 inserts row at end of table
+            const groceryCellName = groceryRow.insertCell(0);
+            const groceryCellCost = groceryRow.insertCell(1);
+            const groceryCellRemove = groceryRow.insertCell(2);
+
+            //Assign the "grocery name" and "grocery price" to their cells
+            groceryCellName.textContent = name;
+            groceryCellCost.textContent = costNumFixString;
+
+            //Add an "x" to the third cell. This is the "delete row" "x."
+            groceryCellRemove.textContent = "x";
+
+            //Add class names to the three cells
+            groceryCellName.className = "grocery-name";
+            groceryCellCost.className = "grocery-cost";
+            groceryCellRemove.className = "grocery-remove";
+
+            //Invoke the runTheNumbers function
+            market.runTheNumbers();
         }
-    } //close checkTheNumbers function
+    },
 
+    runTheNumbers: function () {
+        "use strict";
 
-    //create row and cells, insert grocery name and price
-    function retrieveTheNumbers (n, c) {
+        //Gather all the elements containing the "grocery-cost" class
+        const groceryCost = document.getElementsByClassName("grocery-cost");
 
-        //receive name and cost from previous function
-        var name2 = (n);
-        var cost2 = (c);
-
-        //retrieve the table, insert a row and three cells into it
-        var groceryTable = document.getElementById("output-grocery-list");
-        var groceryRow = groceryTable.insertRow(-1); //-1 inserts row at end of table
-        var groceryCellName = groceryRow.insertCell(0);
-        var groceryCellCost = groceryRow.insertCell(1);
-        var groceryCellRemove = groceryRow.insertCell(2);
-
-        //retrive the text for name and cost, insert into their cells
-        groceryCellName.textContent = name2;
-        groceryCellCost.textContent = cost2;
-        groceryCellRemove.textContent = "x";
-
-        //add a class name to the grocery name, cost, and remove cells
-        groceryCellName.className = "grocery-name";
-        groceryCellCost.className = "grocery-cost";
-        groceryCellRemove.className = "grocery-remove";
-
-        runTheNumbers();
-
-    } //close retrieveTheNumbers function
-
-
-    //add the numbers, retrieve, and insert total cost
-    function runTheNumbers () {
-        //Gather all the elements containing the grocery-cost class
-        var groceryCost = document.getElementsByClassName("grocery-cost");
-
-        //if no groceries on list, reload the page. This is in case user deletes all groceries on list. Total is erased. Placeholder values on input fields are returned.
+        //If no "grocery-cost" class exists, reload the page. This is in case user deletes all groceries on list. Total is erased. Placeholder values on input fields are returned.
         if (groceryCost.length <= 0) {
             location.reload();
         } else {
             //Convert the resulting "array like" NodeList to a "real" Array and retrieve its textContent
-            var text = Array.from(groceryCost, (groceryCost2) => groceryCost2.textContent);
+            const text = Array.from(groceryCost, (groceryCost2) => groceryCost2.textContent);
 
             //Convert string to numbers with "map"
-            var numbers = text.map(Number);
+            const numbers = text.map(Number);
 
-            //add the numbers with "reduce"
-            function consolidate (total, currentValue) {
-                return total + currentValue;
-            }
+            //Add the numbers with "reduce"
+            const sumTotal = numbers.reduce((acc, val) => acc + val);
 
-            function addTheNumbers () {
-                var sumTotal = numbers.reduce(consolidate);
-                var sumTotalFixed = sumTotal.toFixed(2);
-                document.getElementById("output-total").textContent = sumTotalFixed;
-            } //close addTheNumbers function
+            //Add two spots after decimal so total appearance is consistent: X.XX
+            const sumTotalFixed = sumTotal.toFixed(2);
 
-            addTheNumbers();
-            clearTheBoxes();
+            //Insert the total in the cell
+            document.getElementById("output-total").textContent = sumTotalFixed;
+        }
 
-        } //close if/else statement
+        //Invoke the clearTheBoxes function
+        market.clearTheBoxes();
+    },
 
-    } //close runTheNumbers function
+    clearTheBoxes: function () {
+        "use strict";
 
+        //Gather and then clear text from input fields
+        let index;
+        const clear = document.querySelectorAll("#input-grocery-name, #input-grocery-cost");
 
-    //gather and clear text from input fields
-    function clearTheBoxes () {
-        var index = 0;
-        var clear = document.querySelectorAll("#input-grocery-name, #input-grocery-cost");
-
-        for (index; index < clear.length; index++) {
+        for (index = 0; index < clear.length; index += 1) {
             clear[index].textContent = "";
         }
 
-        //return cursor to the grocery name field
+        //Return cursor to the "grocery name" field
         document.getElementById("input-grocery-name").focus();
+    },
 
-    } //close clearTheBoxes function
+    removeTheRow: function () {
+        "use strict";
 
+        //Get the table
+        const list = document.getElementById("output-grocery-list");
 
-    //remove the row, if desired
-    var list = document.getElementById("output-grocery-list");
+        //Monitor the table for a "click" event
+        list.addEventListener("click", function (element) {
 
-    list.addEventListener("click", function (element) {
+            const listRow = element.target.parentNode.rowIndex;
+            const listCell = element.target.cellIndex;
 
-        var listRow = element.target.parentNode.rowIndex;
-        var listCell = element.target.cellIndex;
+            //If the "click" event occurs on the "third" cell (remember, the first cell is 0), delete the row
+            if (listCell === 2) {
+                list.deleteRow(listRow);
 
-        //only remove the row if target is cell 2 (remember, the first cell is 0)
-        if (listCell === 2) {
-            list.deleteRow(listRow);
+                //Invoke the runTheNumbers function
+                market.runTheNumbers();
+            }
+        });
+    }
 
-            runTheNumbers();
-        }
-    });
-
-}); //close DOMContentLoaded function
+}; //close market
