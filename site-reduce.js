adefa4dffb58cefbee6631dd6eedae21c840decc
Sean Kelliher <sbkelliher@icloud.com>
2020-04-07 16:39:16 -0400

    Modify readyKeys function.

--- site-reduce.js
+++ site-reduce.js
@@ -27,16 +27,17 @@ market = {
     readyKeys: function () {
         "use strict";
 
-        const input = document.getElementById("input-grocery");
+        //Get the dialogue element (where users input name and price).
+        const dialogue = document.querySelector("dialogue");
 
-        //Monitor the keyboard for "keydown" event.
-        input.addEventListener("keydown", function (event) {
+        //Monitor dialogue element for "keydown" event.
+        dialogue.addEventListener("keydown", function (event) {
 
-            //If the "keydown" event is on the "return" key...
-            //prevent default action, invoke the retrieveTheNumbers function.
+            //If the "keydown" event is on the "return" key, prevent default.
+            //Invoke function to check format of entered name and price.
             if (event.keyCode === 13) {
                 event.preventDefault();
-                market.checkInsertContent();
+                market.checkEntered();
             }
         });
     },
